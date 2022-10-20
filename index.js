6d5ba4dd25a17e4783085a36cbd8985159dcb3ca
Willian Arenas <williamarenas07@gmail.com>
2022-10-20 15:54:51 -0400

    Calculator exercise

---
+++ index.js
@@ -0,0 +1,101 @@
+"use strict";
+
+window.onload = init;
+
+function init() {
+    const addBtn = document.getElementById("addBtn");
+    addBtn.onclick = onAddBtnClicked;
+
+    const subtractBtn = document.getElementById("subtractBtn");
+    subtractBtn.onclick = onSubtractBtnClicked;
+
+    const multiplyBtn = document.getElementById("multiplyBtn");
+    multiplyBtn.onclick = onMultiplyBtnClicked;
+
+    const divideBtn = document.getElementById("divideBtn");
+    divideBtn.onclick = onDivideBtnClicked;
+}
+
+function onAddBtnClicked() {
+    const number1Field = document.getElementById("number1Field");
+    const number2Field = document.getElementById("number2Field");
+    const messagePara = document.getElementById("messagePara");
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    if (isNaN(number1) || isNaN(number2)) {
+        messagePara.innerHTML =
+            "One or more of your input values are invalid";
+        return;
+    }
+
+    let answer = number1 + number2;
+
+    const answerField = document.getElementById("answerField");
+
+    answerField.value = answer;
+}
+
+function onSubtractBtnClicked() {
+    const number1Field = document.getElementById("number1Field");
+    const number2Field = document.getElementById("number2Field");
+    const messagePara = document.getElementById("messagePara");
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    if (isNaN(number1) || isNaN(number2)) {
+        messagePara.innerHTML =
+            "One or more of your input values are invalid";
+        return;
+    }
+
+    let answer = number1 - number2;
+
+    const answerField = document.getElementById("answerField");
+
+    answerField.value = answer;
+}
+
+function onMultiplyBtnClicked() {
+    const number1Field = document.getElementById("number1Field");
+    const number2Field = document.getElementById("number2Field");
+    const messagePara = document.getElementById("messagePara");
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    if (isNaN(number1) || isNaN(number2)) {
+        messagePara.innerHTML =
+            "One or more of your input values are invalid";
+        return;
+    }
+
+    let answer = number1 * number2;
+
+    const answerField = document.getElementById("answerField");
+
+    answerField.value = answer;
+}
+
+function onDivideBtnClicked() {
+    const number1Field = document.getElementById("number1Field");
+    const number2Field = document.getElementById("number2Field");
+    const messagePara = document.getElementById("messagePara");
+
+    let number1 = Number(number1Field.value);
+    let number2 = Number(number2Field.value);
+
+    if (isNaN(number1) || isNaN(number2)) {
+        messagePara.innerHTML =
+            "One or more of your input values are invalid";
+        return;
+    }
+
+    let answer = number1 / number2;
+
+    const answerField = document.getElementById("answerField");
+
+    answerField.value = answer;
+}
